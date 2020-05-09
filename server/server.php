4b3548827bbf49e038dac2bd70f74f91f7be28c4
hi101000 <60079731+hi101000@users.noreply.github.com>
2020-05-08 21:38:12 -0400

    added some db stuff

--- server/server.php
+++ server/server.php
@@ -1,3 +1,17 @@
 <?php
-	
+	$dbhost='localhost:3036';
+	$dbuser='root';
+	$dbpass='rootpassword';
+	$conn=mysql_connect($dbhost, $dbuser, $dbpass)
+	if(! $conn){
+		die('Could not create database: ' . mysql_error());
+	}
+	echo 'connected successfully<br />'
+	$sql='CREATE DATABASE TUTUORIALS';
+	$retval=mysql_query($sql, $conn);
+	if(! $retval ) {
+            die('Could not create database: ' . mysql_error());
+         }
+    echo "Database TUTORIALS created successfully\n";
+    mysql_close($conn);
 ?>
